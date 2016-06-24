9fb93753f73d18eb5384a5a455a0041eeb9fccf8
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2016-06-24 10:14:10 -0300

    Using net/http client (initial implementation)

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"github.com/valyala/fasthttp"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -14,12 +21,12 @@ var (
 	compress             = flag.Bool("compress", false, "Whether to enable transparent response compression")
 )
 
-var upstreamClient *fasthttp.HostClient
+var upstreamClient *http.Client
 
 func main() {
 	flag.Parse()
 
-	upstreamClient = newClient()
+	upstreamClient = &http.Client{Timeout: 15 * time.Second}
 
 	h := requestHandler
 	if *compress {
@@ -33,19 +40,37 @@ func main() {
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
 
-	err := upstreamClient.DoTimeout(&ctx.Request, &ctx.Response, 15 * time.Second)
+	headers := http.Header{}
+	ctx.Request.Header.VisitAll(func(key, value []byte) {
+		headers.Add(string(key), string(value))
+	})
+	u, err := url.Parse(fmt.Sprint("http://", *upstreamHost, "/", string(ctx.Request.RequestURI())))
 	if err != nil {
 		ctx.Error("Bad Gateway", fasthttp.StatusBadGateway)
 		log.Print(err)
 		return
 	}
-
-}
-
-func newClient() *fasthttp.HostClient {
-	return &fasthttp.HostClient{
-		Addr:                          *upstreamHost,
-		MaxConns:                      *maxIdleUpstreamConns,
-		DisableHeaderNamesNormalizing: true,
+	req := &http.Request{
+		Method:        string(ctx.Method()),
+		URL:           u,
+		Proto:         map[bool]string{true: "HTTP/1.1", false: "HTTP/1.0"}[ctx.Request.Header.IsHTTP11()],
+		ProtoMinor:    map[bool]int{true: 1, false: 0}[ctx.Request.Header.IsHTTP11()],
+		Header:        headers,
+		Body:          ioutil.NopCloser(bytes.NewReader(ctx.Request.Body())),
+		ContentLength: int64(ctx.Request.Header.ContentLength()),
+		Host:          string(ctx.Request.Header.Host()),
 	}
+
+	resp, err := upstreamClient.Do(req)
+	if err != nil {
+		ctx.Error("Bad Gateway", fasthttp.StatusBadGateway)
+		log.Print(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	for k, v := range resp.Header {
+		ctx.Response.Header.Add(k, strings.Join(v, ","))
+	}
+	io.Copy(ctx.Response.BodyWriter(), resp.Body)
 }
